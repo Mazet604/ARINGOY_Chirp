6f14d2ae3bab5fbfd38c1d16cdcabcfbead23255
Loid Aringoy <loidandre30@gmail.com>
2024-12-04 13:40:20 +0800

    fully working notifs

--- resources/js/Layouts/AuthenticatedLayout.vue
+++ resources/js/Layouts/AuthenticatedLayout.vue
@@ -37,7 +37,6 @@ const showingNavigationDropdown = ref(false);
                                 </NavLink>
                             </div>
                         </div>
-
                         <div class="hidden sm:flex sm:items-center sm:ms-6">
                             <!-- Settings Dropdown -->
                             <div class="ms-3 relative">
